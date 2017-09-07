671b0fe11a97ba499d3ad60901b2007a8f6d4e48
François Pluchino <francois.pluchino@gmail.com>
2017-09-07 17:42:06 +0200

    Add phpunit

---
+++ Tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Foxy package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__.'/../vendor/autoload.php';
+
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
