7b8f611e6856c6bf43e6a20342ace64ef5df6254
Sam <sam@mousa.nl>
2021-06-11 11:29:28 +0200

    Fixed compatibility with composer 2.1

--- Util/LockerUtil.php
+++ Util/LockerUtil.php
@@ -36,7 +36,7 @@ class LockerUtil
     {
         $lockFile = str_replace('.json', '.lock', $composerFile);
         // @codeCoverageIgnoreStart
-        return \defined('Composer\Composer::RUNTIME_API_VERSION') && '2.0.0' === Composer::RUNTIME_API_VERSION
+        return \defined('Composer\Composer::RUNTIME_API_VERSION') && in_array(Composer::RUNTIME_API_VERSION, ['2.0.0', '2.1.0'])
             ? new Locker($io, new JsonFile($lockFile, null, $io), $im, file_get_contents($composerFile))
             : new Locker($io, new JsonFile($lockFile, null, $io), $rm, $im, file_get_contents($composerFile));
     }
